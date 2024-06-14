19e00e1eb65e0c90b53548987da380b0ccad41e3
Anderson Filippi <andersonfilippi@gmail.com>
2024-06-14 18:44:46 -0300

    exemplos com ponteiros em diversos cenários

--- ponteiros.go
+++ ponteiros.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
+type Carro struct {
+	Name string
+}
+
+func (c Carro) andou() {
+	// sempre que o método andou for chamado, o nome será BMW
+	c.Name = "BMW"
+	fmt.Println(c.Name, "andou")
+}
+
+func (c *Carro) andou2() {
+	// sempre que o método andou for chamado, o nome será BMW
+	// inclusive no ponteiro. fora deste escopo.
+	c.Name = "BMW"
+	fmt.Println(c.Name, "andou")
+}
+
 func main() {
 
 	// memoria-endereco(40) <--- a <--- 40
@@ -24,4 +41,39 @@ func main() {
 
 	// faz o de-reference, buscando o valor que está contido no endereço de "a"
 	fmt.Println("valor de \"a\" usando de-reference do ponteiro", *ponteiro)
+
+	// mudando o valor de "a" usando o ponteiro
+	*ponteiro = 50
+	fmt.Println("novo valor do ponteiro via de-reference:", *ponteiro)
+	fmt.Println("valor de \"a\":", a)
+
+	// exemplo de alteração de valor de uma variável usando uma função sem retorno
+	variavel := 10
+	fmt.Println("valor variavel:", variavel)
+	abc(&variavel)
+	fmt.Println("novo valor variavel:", variavel)
+
+	// exemplo de método com alteração de variáveis em escopos diferentes
+	carro := Carro{
+		Name: "Ka",
+	}
+	// vai imprimir o BMW
+	carro.andou()
+	// aqui vai imprmir o nome do carro que foi atribuído ao invés de imprimir
+	// o nome que está no método andou()
+	fmt.Println("nome do carro:", carro.Name)
+
+	// agora o exemplo usando um ponteiro
+	carro2 := Carro{
+		Name: "Ka",
+	}
+	// vai imprimir o BMW
+	carro2.andou2()
+	// também vai imprimir o BMW
+	fmt.Println("nome do carro2:", carro2.Name)
+
+}
+
+func abc(a *int) {
+	*a = 200
 }
